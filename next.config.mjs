acaa6685fe65ec52cd1b46516a4139ef90aaa565
TGS963 <ghoshsuvojit2012@gmail.com>
2024-02-29 23:58:52 +0530

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   basePath: "/tgs963.github.io",
   output: "export",
   reactStrictMode: true,
+  images: {
+    domains: ["tgs963.github.io", "github.com"],
+  },
 };
 
 export default nextConfig;
